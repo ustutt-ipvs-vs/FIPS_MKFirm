bb65a2c322458315d4de9fe646f3aba19bec65e8
Simon Egger <st160727@stud.uni-stuttgart.de>
2024-12-23 11:31:54 +0100

    Add AGV network builder & heuristic benchmark

---
+++ benchmarks/heuristic.cc
@@ -0,0 +1,33 @@
+#include "dgm/transmission_graph.h"
+#include "network/stream_storage.h"
+#include "network/topology.h"
+#include "solver/initial.h"
+#include <argparse/argparse.hpp>
+#include <print>
+
+using namespace tsndgm;
+
+int main(int argc, char **argv) {
+  argparse::ArgumentParser program(
+      "Heuristics for Wireless IEEE 802.1Qbv Scheduling");
+  program.add_argument("-n", "--network")
+      .default_value(std::string("../data/network.json"))
+      .required()
+      .help("specify the network topology");
+  program.add_argument("-s", "--streams")
+      .default_value(std::string("../data/streams.json"))
+      .required()
+      .help("specify the time-triggered streams");
+  program.parse_args(argc, argv);
+
+  auto network_file = std::filesystem::path(program.get<std::string>("-n"));
+  auto network = NetworkTopology(network_file);
+  auto stream_file = std::filesystem::path(program.get<std::string>("-s"));
+  auto streams = StreamStorage(stream_file, network);
+
+  auto g = TransmissionGraph::build_from_heuristic<EffectiveRelease>(&streams);
+  g.critical_path();
+  g.print_critical_path();
+
+  return 0;
+}
---
+++ scripts/agv_network_builder.py
@@ -0,0 +1,554 @@
+import argparse
+import math
+import random
+import itertools
+import json
+import subprocess
+import sys
+
+random.seed(1234)
+RTI_POLICIES = {
+    "minimize_interval": 0,  # more scheduling flexibility
+    "minimize_dmax": 1,  # better suited for emergency traffic
+}  # chose "minimize_dmax" iff using emergency traffic
+
+STREAM_OBJECTIVES = {
+    "lateness": 0,  # more scheduling flexibility
+    "tardiness": 1,  # better suited for emergency traffic
+}  # chose "minimize_dmax" iff using emergency traffic
+
+DATA_RATE = 100000000  # 100Mbps
+PROPAGATION_DELAY = 50  # 50ns (~10m Ethernet cable)
+PROCESSING_DELAY = 0
+
+CT_PERIOD = 1000000  # 1ms
+CT_PHASE = 0
+CT_FRAMESIZE = 100  # 100 byte
+CT_E2E_LATENCY = 500000  # 500us
+CT_JITTER = 250000  # 250us
+CT_PCP = 7
+
+WT_PERIOD = 25000000  # 25ms
+WT_PHASE = 0
+WT_FRAMESIZE = 100  # 100 byte
+WT_E2E_LATENCY = 25000000  # 20ms
+WT_JITTER = 5000000  # 5ms
+WT_PCP = 7
+
+WT_RELIABILITY = 0.9999
+WT_FRAME_LOSS = 0
+WT_RTI_POLICY = "minimize_dmax"
+STREAM_OBJECTIVE = "tardiness"
+
+OMNETPP_X = 700
+OMNETPP_Y = 500
+
+offset = 0
+network = {"nodes": [], "links": []}
+streams = []
+
+device_type = {"END_DEVICE": 0, "TSN_BRIDGE": 1, "TSN_TRANSLATOR": 2, "UNSPECIFIED": 3}
+
+
+def random_path(size: int, offset: int):
+    talker = random.randrange(2 ** (size - 1) - 1, 2**size - 1)
+    path = [talker]
+    while path[-1] != 0:
+        path.append(math.floor((path[-1] - 1) / 2))
+
+    path = [i + offset for i in path]
+    return path
+
+
+def build_core(size: int, ct: int, bypass: bool):
+    global offset
+
+    for node in range(0, 2**size - 1):
+        xpos = 0
+        ypos = 0
+        node_type = "UNSPECIFIED"
+        if node == 0:
+            xpos = OMNETPP_X
+            ypos = OMNETPP_Y
+            node_type = "TSN_TRANSLATOR"
+        elif node < 2 ** (size - 1) - 1:
+            level = math.floor(math.log2(node + 1))
+            xpos = OMNETPP_X + 100 * level
+            ypos = int(
+                OMNETPP_Y
+                + 100
+                * 2 ** (size - 1 - level)
+                * (node - 2**level - 2 ** (level - 1) + 1.5)
+            )
+            node_type = "TSN_BRIDGE"
+        else:
+            level = math.floor(math.log2(node + 1))
+            xpos = OMNETPP_X + 100 * level
+            ypos = int(OMNETPP_Y + 100 * (node - 2**level - 2 ** (level - 1) + 1.5))
+            node_type = "END_DEVICE"
+
+        # device
+        network["nodes"].append(
+            {
+                "id": offset + node,
+                "name": f"CORE_{str(node).zfill(2)}",
+                "processing_delay": PROCESSING_DELAY,
+                "type": device_type[node_type],
+                "position": {"x": xpos, "y": ypos},
+            }
+        )
+
+        # links
+        if node > 0:
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "source": offset + node,
+                    "target": offset + math.floor((node - 1) / 2),
+                }
+            )
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "target": offset + node,
+                    "source": offset + math.floor((node - 1) / 2),
+                }
+            )
+        if node == 2:
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "target": offset + 1,
+                    "source": offset + 2,
+                }
+            )
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "target": offset + 2,
+                    "source": offset + 1,
+                }
+            )
+
+    # cross traffic
+    for s in range(0, ct):
+        path_a = (
+            random_path(size, offset) if not bypass else random_path(size, offset)[:-1]
+        )
+        path_b = []
+        while len(path_b) == 0:
+            path_b = (
+                random_path(size, offset)
+                if not bypass
+                else random_path(size, offset)[:-1]
+            )
+            path_b = [x for x in path_b if x not in path_a]
+        path_a = path_a[: len(path_b) + 1]
+        path = path_a + list(reversed(path_b))
+        route = list(itertools.pairwise(path))
+
+        streams.append(
+            {
+                "name": f"CORE_CT{str(s).zfill(2)}",
+                "period": CT_PERIOD,
+                "phase": CT_PHASE,
+                "pcp": CT_PCP,
+                "objective_type": STREAM_OBJECTIVES[STREAM_OBJECTIVE],
+                "e2e_latency": CT_E2E_LATENCY,
+                "jitter": CT_JITTER,
+                "frame_size": CT_FRAMESIZE,
+                "route": route,
+                "pdb_map": None,
+                "weight": 1.0,
+            }
+        )
+
+    offset += 2**size - 1
+
+
+def write_histograms(pdc, vslot):
+    if pdc < 0:
+        return
+
+    name = str(pdc * 100).replace(".", "-")
+
+    subprocess.run(
+        [
+            "python",
+            "scripts/histogram_manipulation.py",
+            "-hist",
+            "data/histograms/uplink_histogram.json",
+            "-pdc",
+            str(pdc),
+            "-vslot",
+            str(vslot),
+            "-name",
+            f"pdc_uplink_{name}_{vslot}",
+            "-save",
+        ]
+    )
+    subprocess.run(
+        [
+            "python",
+            "scripts/histogram_manipulation.py",
+            "-hist",
+            "data/histograms/downlink_histogram.json",
+            "-pdc",
+            str(pdc),
+            "-vslot",
+            str(vslot),
+            "-name",
+            f"pdc_downlink_{name}_{vslot}",
+            "-save",
+        ]
+    )
+
+
+def build_agv(
+    n: int,
+    size: int,
+    csize: int,
+    wt_in: int,
+    wt_out: int,
+    ct: int,
+    rel: float,
+    bypass: bool,
+    pdc: float,
+    vslot: int,
+):
+    global offset, OMNETPP_Y
+
+    pdc_name = str(pdc * 100).replace(".", "-")
+
+    for node in range(0, 2**size - 1):
+        xpos = 0
+        ypos = 0
+        node_type = "UNSPECIFIED"
+        if node == 0:
+            xpos = OMNETPP_X - 200
+            ypos = OMNETPP_Y
+            node_type = "TSN_TRANSLATOR"
+        elif node < 2 ** (size - 1) - 1:
+            level = math.floor(math.log2(node + 1))
+            xpos = OMNETPP_X - 100 * (level + 2)
+            ypos = int(
+                OMNETPP_Y
+                + 100
+                * 2 ** (size - 1 - level)
+                * (node - 2**level - 2 ** (level - 1) + 1.5)
+            )
+            node_type = "TSN_BRIDGE"
+        else:
+            level = math.floor(math.log2(node + 1))
+            xpos = OMNETPP_X - 100 * (level + 2)
+            ypos = int(OMNETPP_Y + 100 * (node - 2**level - 2 ** (level - 1) + 1.5))
+            node_type = "END_DEVICE"
+
+        # device
+        network["nodes"].append(
+            {
+                "id": offset + node,
+                "name": f"AGV{n}_{str(node).zfill(2)}",
+                "processing_delay": PROCESSING_DELAY,
+                "type": device_type[node_type],
+                "position": {"x": xpos, "y": ypos},
+            }
+        )
+
+        # links
+        if node > 0:
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "source": offset + node,
+                    "target": offset + math.floor((node - 1) / 2),
+                }
+            )
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "target": offset + node,
+                    "source": offset + math.floor((node - 1) / 2),
+                }
+            )
+        if node == 2:
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "target": offset + 1,
+                    "source": offset + 2,
+                }
+            )
+            network["links"].append(
+                {
+                    "type": 0,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "target": offset + 2,
+                    "source": offset + 1,
+                }
+            )
+        if node == 0:
+            network["links"].append(
+                {
+                    "type": 1,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "multiple_subcarriers": True,
+                    "target": offset,
+                    "source": 0,
+                }
+            )
+            network["links"].append(
+                {
+                    "type": 1,
+                    "data_rate": DATA_RATE,
+                    "propagation_delay": PROPAGATION_DELAY,
+                    "multiple_subcarriers": True,
+                    "target": 0,
+                    "source": offset,
+                }
+            )
+
+    # cross traffic
+    for s in range(0, ct):
+        path_a = (
+            random_path(size, offset) if not bypass else random_path(size, offset)[:-1]
+        )
+        path_b = []
+        while len(path_b) == 0:
+            path_b = (
+                random_path(size, offset)
+                if not bypass
+                else random_path(size, offset)[:-1]
+            )
+            path_b = [x for x in path_b if x not in path_a]
+        path_a = path_a[: len(path_b) + 1]
+        path = path_a + list(reversed(path_b))
+        route = list(itertools.pairwise(path))
+
+        streams.append(
+            {
+                "name": f"AGV{n}_CT{str(s).zfill(2)}",
+                "period": CT_PERIOD,
+                "phase": CT_PHASE,
+                "pcp": CT_PCP,
+                "objective_type": STREAM_OBJECTIVES[STREAM_OBJECTIVE],
+                "e2e_latency": CT_E2E_LATENCY,
+                "jitter": CT_JITTER,
+                "frame_size": WT_FRAMESIZE,
+                "route": route,
+                "pdb_map": None,
+                "weight": 1.0,
+            }
+        )
+
+    # incoming wireless traffic
+    for s in range(0, wt_in):
+        route = (
+            list(itertools.pairwise(random_path(csize, 0)))
+            + [(0, offset)]
+            + list(itertools.pairwise(list(reversed(random_path(size, offset)))))
+        )
+        if pdc > 0:
+            streams.append(
+                {
+                    "name": f"CORE_AGV{n}_{str(s).zfill(2)}",
+                    "period": WT_PERIOD,
+                    "phase": WT_PHASE,
+                    "pcp": WT_PCP,
+                    "objective_type": STREAM_OBJECTIVES[STREAM_OBJECTIVE],
+                    "e2e_latency": WT_E2E_LATENCY,
+                    "jitter": WT_JITTER,
+                    "frame_loss": WT_FRAME_LOSS,
+                    "frame_size": WT_FRAMESIZE,
+                    "route": route,
+                    "pdb_map": [
+                        {
+                            "link": (0, offset),
+                            "reliability": rel,
+                            "policy": RTI_POLICIES[WT_RTI_POLICY],
+                            "histogram": f"../data/modified_histograms/pdc_downlink_{pdc_name}_{vslot}.json",
+                        }
+                    ],
+                    "weight": 1.0,
+                }
+            )
+        else:
+            streams.append(
+                {
+                    "name": f"CORE_AGV{n}_{str(s).zfill(2)}",
+                    "period": WT_PERIOD,
+                    "phase": WT_PHASE,
+                    "pcp": WT_PCP,
+                    "objective_type": STREAM_OBJECTIVES[STREAM_OBJECTIVE],
+                    "e2e_latency": WT_E2E_LATENCY,
+                    "jitter": WT_JITTER,
+                    "frame_loss": WT_FRAME_LOSS,
+                    "frame_size": WT_FRAMESIZE,
+                    "route": route,
+                    "pdb_map": [
+                        {
+                            "link": (0, offset),
+                            "reliability": rel,
+                            "policy": RTI_POLICIES[WT_RTI_POLICY],
+                            "histogram": "../data/histograms/downlink_histogram.json",
+                        }
+                    ],
+                    "weight": 1.0,
+                }
+            )
+
+    # outgoing wireless traffic
+    for s in range(0, wt_out):
+        route = (
+            list(itertools.pairwise(random_path(size, offset)))
+            + [(offset, 0)]
+            + list(itertools.pairwise(list(reversed(random_path(csize, 0)))))
+        )
+        if pdc > 0:
+            streams.append(
+                {
+                    "name": f"AGV{n}_CORE_{str(s).zfill(2)}",
+                    "period": WT_PERIOD,
+                    "phase": WT_PHASE,
+                    "pcp": WT_PCP,
+                    "objective_type": STREAM_OBJECTIVES[STREAM_OBJECTIVE],
+                    "e2e_latency": WT_E2E_LATENCY,
+                    "jitter": WT_JITTER,
+                    "frame_loss": WT_FRAME_LOSS,
+                    "frame_size": WT_FRAMESIZE,
+                    "route": route,
+                    "pdb_map": [
+                        {
+                            "link": (offset, 0),
+                            "reliability": rel,
+                            "policy": RTI_POLICIES[WT_RTI_POLICY],
+                            "histogram": f"../data/modified_histograms/pdc_uplink_{pdc_name}_{vslot}.json",
+                        }
+                    ],
+                    "weight": 1.0,
+                }
+            )
+        else:
+            streams.append(
+                {
+                    "name": f"AGV{n}_CORE_{str(s).zfill(2)}",
+                    "period": WT_PERIOD,
+                    "phase": WT_PHASE,
+                    "pcp": WT_PCP,
+                    "objective_type": STREAM_OBJECTIVES[STREAM_OBJECTIVE],
+                    "e2e_latency": WT_E2E_LATENCY,
+                    "jitter": WT_JITTER,
+                    "frame_loss": WT_FRAME_LOSS,
+                    "frame_size": WT_FRAMESIZE,
+                    "route": route,
+                    "pdb_map": [
+                        {
+                            "link": (offset, 0),
+                            "reliability": rel,
+                            "policy": RTI_POLICIES[WT_RTI_POLICY],
+                            "histogram": "../data/histograms/uplink_histogram.json",
+                        }
+                    ],
+                    "weight": 1.0,
+                }
+            )
+
+    offset += 2**size - 1
+    OMNETPP_Y += 100 * 2 ** (size - 1)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="Robost Network Builder",
+        description="builds a simple network with N AGVs that want to communication with M edge servers",
+    )
+
+    parser.add_argument(
+        "-no_bypass", "--cross_traffic_bypass", default=True, action="store_false"
+    )
+
+    parser.add_argument(
+        "-agvs", "--agv_size_specification", type=int, nargs="+", default=[3]
+    )
+    parser.add_argument(
+        "-agv_wt_out",
+        "--agv_outgoing_wireless_traffic_specification",
+        type=int,
+        nargs="+",
+        default=[30],
+    )
+    parser.add_argument(
+        "-agv_wt_in",
+        "--agv_ingoing_wireless_traffic_specification",
+        type=int,
+        nargs="+",
+        default=[30],
+    )
+    parser.add_argument(
+        "-agv_ct", "--agv_cross_traffic_specification", type=int, nargs="+", default=[5]
+    )
+
+    parser.add_argument("-core", "--core_size_specification", type=int, default=4)
+    parser.add_argument(
+        "-core_ct", "--core_cross_traffic_specification", type=int, default=5
+    )
+
+    parser.add_argument("-rel", "--reliability", type=float, default=WT_RELIABILITY)
+    parser.add_argument("-pdc", "--packet_delay_correction", type=float, default=0)
+    parser.add_argument("-vslot", "--virtual_slot_size", type=int, default=0)
+    parser.add_argument("-suffix", "--suffix", type=str, default="")
+    parser.add_argument("-prefix", "--prefix", type=str, default="data")
+
+    args = parser.parse_args()
+    path_suffix = args.suffix
+    path_prefix = args.prefix
+
+    build_core(
+        args.core_size_specification,
+        args.core_cross_traffic_specification,
+        args.cross_traffic_bypass,
+    )
+    for i in range(len(args.agv_size_specification)):
+        build_agv(
+            i,
+            args.agv_size_specification[i],
+            args.core_size_specification,
+            args.agv_ingoing_wireless_traffic_specification[i],
+            args.agv_outgoing_wireless_traffic_specification[i],
+            args.agv_cross_traffic_specification[i],
+            args.reliability,
+            args.cross_traffic_bypass,
+            args.packet_delay_correction,
+            args.virtual_slot_size,
+        )
+
+    print(f"generated network with {len(streams)} streams")
+
+    with open(f"{path_prefix}/network{path_suffix}.json", "w") as f:
+        json.dump(network, f, indent=4)
+    with open(f"{path_prefix}/streams{path_suffix}.json", "w") as f:
+        json.dump(streams, f, indent=4)
+    if args.packet_delay_correction > 0:
+        write_histograms(args.packet_delay_correction, args.virtual_slot_size)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
